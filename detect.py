6a5ac2842826d3a7315a491940adfdf2d48906b7
SKN443 <92131666+SKN443@users.noreply.github.com>
2023-12-12 15:33:07 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -17,7 +17,7 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized,
 
 def detect(save_img=False):
     source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
-    hide_conf, hide_label = opt.hide_conf, opt.hide_label
+    hide_conf, hide_labels = opt.hide_conf, opt.hide_labels
     save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
@@ -126,7 +126,7 @@ def detect(save_img=False):
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or view_img:  # Add bbox to image
-                        if hide_label:
+                        if hide_labels:
                             if hide_conf:
                                 label = None
                             else: 
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--hide_conf', default=False, help='Hide confident score')
-    parser.add_argument('--hide_label', default=False, help='Hide label')
+    parser.add_argument('--hide_labels', default=False, help='Hide label')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
